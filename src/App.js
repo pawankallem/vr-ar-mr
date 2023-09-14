8d71c412690bf1c3ad87040a6460397d1779d79f
Kallem Pavan <90694105+pawankallem@users.noreply.github.com>
2023-09-14 10:21:19 +0000

    images added in assets folder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import react, {useState, useEffect} from "react";
 import { Page1 } from "./components/page1/Page1";
 import { Page2 } from "./components/page2/Page2";
 import "./styles.css";
 
 export default function App() {
+  const [page,setPage] = useState(1);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setPage(2);
+    },9000)
+  })
   return (
     <div className="App">
-      <Page1 />
+      {
+        page === 1 ? <Page1/> : <Page2/>
+      }
+      {/* <Page1 /> */}
       {/* <Page2 /> */}
     </div>
   );
